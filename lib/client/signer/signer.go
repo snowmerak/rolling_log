82c5d41312a6fe75b1a2a30731ebfd1ddc6544a7
snowmerak <snowmerak@outlook.com>
2024-12-29 05:33:41 +0900

    fix signature

--- lib/client/signer/signer.go
+++ lib/client/signer/signer.go
@@ -1,5 +1,22 @@
 package signer
 
+import "errors"
+
 type Signer interface {
-	Sign(data []byte) []byte
+	Sign(data []byte) ([]byte, error)
+	Marshal() ([]byte, error)
+	Verifier() (Verifier, error)
+	Unmarshal(data []byte) (Signer, error)
 }
+
+type Verifier interface {
+	Verify(data []byte, signature []byte) (bool, error)
+	Marshal() ([]byte, error)
+	Unmarshal(data []byte) (Verifier, error)
+}
+
+var (
+	ErrInvalidPublicKey = errors.New("invalid public key")
+	ErrInvalidSignature = errors.New("invalid signature")
+	ErrInvalidData      = errors.New("invalid data")
+)
